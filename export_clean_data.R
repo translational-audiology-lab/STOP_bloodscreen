463c25646057129db26e1477c073a2fb901b0cc9
Mun-Gwan Hong <mungwan.hong@nbis.se>
2021-05-31 10:22:44 +0200

    Export cleaned data as the user's request

---
+++ export_clean_data.R
@@ -0,0 +1,45 @@
+# -----------------------------------------------------------------------------#
+# Extract cleaned clinical info and proteomic data
+# -----------------------------------------------------------------------------#
+# initiated on 2021-05-30
+# authors :  Mun-Gwan Hong
+# -----------------------------------------------------------------------------#
+rm(list = ls())
+
+# Please note some functions are called directly using '::' or ':::'
+library(tidyverse)
+
+#----- File names --------------------------------------------------------------
+
+fn <- list(
+  lib = "utils.R",
+  i = list(                               #  input
+    olk02 = "../data/s1-olink_proteomic.v02.RData",
+    c02 = "../data/s1-clinical.v02.RData"
+  ),
+  o = list(                               #  output
+    ut = "../data/exported_clean_data.xlsx"
+  )
+)
+
+# Brief check if all files exist
+stopifnot(all(file.exists(fn$lib, unlist(fn$i), dirname(unlist(fn$o)))))
+
+#----- MAIN --------------------------------------------------------------------
+
+# Load data
+load(fn$i$olk02) # proteomic data 
+load(fn$i$c02)  # clinical information
+# confirm no missing samples
+stopifnot(all(qns$SampleID %in% olink$SampleID))
+stopifnot(all(olink$SampleID %in% qns$SampleID))
+
+# Olink data in wide form
+olink_wide <- olink %>% 
+  unite(prot_olinkid, Assay, OlinkID, sep = " (") %>% 
+  mutate(prot_olinkid = paste0(prot_olinkid, ")")) %>% 
+  pivot_wider(SampleID, names_from = prot_olinkid, values_from = NPX)
+# Olink wide form + clinical info
+oc <- full_join(olink_wide, qns, by = "SampleID")
+
+openxlsx::write.xlsx(oc, file = fn$o$ut)
